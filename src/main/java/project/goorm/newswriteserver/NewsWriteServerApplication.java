312049d84ff96cb9ff0c4e9b6139a3bcc6b3dee1
Bangseungjae <tmdwo5825@naver.com>
2023-04-01 00:43:18 +0900

    first commit

---
+++ src/main/java/project/goorm/newswriteserver/NewsWriteServerApplication.java
@@ -0,0 +1,13 @@
+package project.goorm.newswriteserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsWriteServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsWriteServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/goorm/newswriteserver/NewsWriteServerApplicationTests.java
@@ -0,0 +1,13 @@
+package project.goorm.newswriteserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsWriteServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
