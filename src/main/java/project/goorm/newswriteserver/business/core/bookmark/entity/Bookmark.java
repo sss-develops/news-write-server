26549b021c5e261a5f5171a86854ad673212b39a
Bangseungjae <tmdwo5825@naver.com>
2023-04-01 01:02:16 +0900

    feature: 기본 엔티티를 작성 - News Entity 작성 - Bookmark Entity 작성 - Deleted를 News Entity에서 EnumType.STRING으로 사용

---
+++ src/main/java/project/goorm/newswriteserver/business/core/bookmark/entity/Bookmark.java
@@ -0,0 +1,61 @@
+package project.goorm.newswriteserver.business.core.bookmark.entity;
+
+import javax.persistence.*;
+import java.time.Instant;
+import java.util.Objects;
+
+@Entity
+public class Bookmark {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bookmarkId;
+
+    @Column
+    private Long newsId;
+
+    @Column
+    private Long memberId;
+
+    @Column
+    private Instant createdAt;
+
+    /**
+     * @Nullary-Constructor. JPA 기본 생성자로 bookmark 외부 패키지에서 호출하지 말 것.
+     */
+    protected Bookmark() {
+    }
+
+    public Long getBookmarkId() {
+        return bookmarkId;
+    }
+
+    public Long getNewsId() {
+        return newsId;
+    }
+
+    public Long getMemberId() {
+        return memberId;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Bookmark bookmark)) return false;
+        return getBookmarkId().equals(bookmark.getBookmarkId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getBookmarkId());
+    }
+
+    @Override
+    public String toString() {
+        return bookmarkId.toString();
+    }
+}
