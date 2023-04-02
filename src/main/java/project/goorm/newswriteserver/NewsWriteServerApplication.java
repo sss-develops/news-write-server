df00256519c71570c13bd80ff098a3f239952f4b
Bangseungjae <tmdwo5825@naver.com>
2023-04-02 23:46:06 +0900

    build: Eureka server에 추가하기 위한 어노테이션 추가

--- src/main/java/project/goorm/newswriteserver/NewsWriteServerApplication.java
+++ src/main/java/project/goorm/newswriteserver/NewsWriteServerApplication.java
@@ -2,7 +2,13 @@ package project.goorm.newswriteserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@RefreshScope
+@EnableFeignClients
+@EnableEurekaClient
 @SpringBootApplication
 public class NewsWriteServerApplication {
 
