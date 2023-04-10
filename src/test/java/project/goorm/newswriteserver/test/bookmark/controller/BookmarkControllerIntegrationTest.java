51cdca2554b15f94e38dd58d0875b7a50b39f44e
Bangseungjae <tmdwo5825@naver.com>
2023-04-10 17:09:24 +0900

    test: REST DOCS를 위한 로직을 추가한다.

--- src/test/java/project/goorm/newswriteserver/test/bookmark/controller/BookmarkControllerIntegrationTest.java
+++ src/test/java/project/goorm/newswriteserver/test/bookmark/controller/BookmarkControllerIntegrationTest.java
@@ -3,38 +3,40 @@ package project.goorm.newswriteserver.test.bookmark.controller;
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.client.WireMock;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 import project.goorm.newswriteserver.business.core.news.entity.News;
-import project.goorm.newswriteserver.business.web.presentation.BookmarkCommandAPI;
-import project.goorm.newswriteserver.common.rdb.AbstractContainerTestBase;
 import project.goorm.newswriteserver.common.rdb.DatabaseTestBase;
 import project.goorm.newswriteserver.test.helper.fixture.NewsFixture;
 import project.goorm.newswriteserver.test.helper.persistence.PersistenceHelper;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 
 @ActiveProfiles({"test"})
 @DisplayName("북마크 API 통합 테스트")
+@ExtendWith(RestDocumentationExtension.class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 public class BookmarkControllerIntegrationTest extends DatabaseTestBase {
 
     private MockMvc mockMvc;
-
-    @Autowired
-    private BookmarkCommandAPI bookmarkCommandAPI;
 
     static final int PORT = 8091;
     public static WireMockServer wireMockServer = new WireMockServer(options().port(PORT));
@@ -61,9 +63,10 @@ public class BookmarkControllerIntegrationTest extends DatabaseTestBase {
     }
 
     @BeforeEach
-    public void setup() {
+    public void setup(WebApplicationContext webApplicationContext, RestDocumentationContextProvider documentation) {
         this.mockMvc = MockMvcBuilders
-                .standaloneSetup(bookmarkCommandAPI)
+                .webAppContextSetup(webApplicationContext)
+                .apply(documentationConfiguration(documentation))
                 .build();
     }
 
@@ -82,10 +85,16 @@ public class BookmarkControllerIntegrationTest extends DatabaseTestBase {
         News news = persistenceHelper.persist(NewsFixture.createNews());
         Long newsId = news.getNewsId();
 
-        ResultActions response = mockMvc.perform(MockMvcRequestBuilders.post("/api/bookmark/save/1/"+newsId)
+        ResultActions response = mockMvc.perform(RestDocumentationRequestBuilders.post("/api/bookmark/save/{memberId}/{newsId}", 1L, newsId)
                 .contentType(MediaType.APPLICATION_JSON)
         );
 
-        response.andExpect(MockMvcResultMatchers.status().isCreated());
+        response.andExpect(MockMvcResultMatchers.status().isCreated())
+                .andDo(document("post-create",
+                        pathParameters(
+                                parameterWithName("memberId").description("member id"),
+                                parameterWithName("newsId").description("news id")
+                        )
+                ));
     }
 }
